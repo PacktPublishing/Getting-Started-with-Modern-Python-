cd627d411375feb16ea0071f58380bd678d31769
Sharan <sharanjeetk@packt.com>
2019-05-06 12:00:00 +0530

    Code added
    Code added

---
+++ Section 4/Section 4/generators_primes.py
@@ -0,0 +1,16 @@
+from future_functions import is_prime
+import time
+
+start_time = time.time()
+primes = (x for x in range(1000000,1200000) if is_prime(x))
+
+print("Primes:",primes)
+print(f"Took {time.time()-start_time:0.2f} seconds")
+
+kept_primes = []
+for prime in primes:
+    if input(f"keep {prime}?").lower().startswith("y"):
+        kept_primes.append(prime)
+        if len(kept_primes) >= 4:
+            break
+print("Keeping",kept_primes)
